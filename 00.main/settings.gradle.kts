b3b5300dd4101a712757cfb1911ded0b8fa20ead
Denis535 <dddenisss35@gmail.com>
2025-11-25 10:35:14 +0200

    Initial commit

---
+++ 00.main/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    this.repositories {
+        this.gradlePluginPortal()
+        this.mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    this.repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    this.repositories {
+        this.mavenCentral()
+    }
+}
+
+plugins {
+    this.id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+rootProject.name = "main"
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    this.repositories {
+        this.gradlePluginPortal()
+        this.mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    this.repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    this.repositories {
+        this.mavenCentral()
+    }
+}
+
+plugins {
+    this.id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+rootProject.name = "kotlin-game-example"
+
+include("main")
+include("ui")
+include("app")
+include("game")
