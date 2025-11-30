cc0a1ee2acd900427be2eaf3723bac195da30a87
Denis535 <dddenisss35@gmail.com>
2025-11-30 17:43:42 +0200

    Engine

--- 10.common/src/main/kotlin/com/denis535/kotlin_game_example/Engine.kt
+++ 10.common/src/main/kotlin/com/denis535/kotlin_game_example/Engine.kt
@@ -12,7 +12,7 @@ public class Engine : AutoCloseable {
             field = value
         }
 
-    public var NumberOfFrame: Int = 0
+    public var Tick: Int = 0
         get() {
             check(this.IsRunning)
             return field
@@ -20,6 +20,38 @@ public class Engine : AutoCloseable {
         private set(value) {
             check(this.IsRunning)
             field = value
+        }
+
+    public var Time: Double = 0.0
+        get() {
+            check(this.IsRunning)
+            return field
+        }
+        private set(value) {
+            check(this.IsRunning)
+            field = value
+        }
+
+    private var PrevTime: Double? = null
+        get() {
+            check(this.IsRunning)
+            return field
+        }
+        set(value) {
+            check(this.IsRunning)
+            field = value
+        }
+
+    public val DeltaTime: Double
+        get() {
+            check(this.IsRunning)
+            return this.PrevTime?.let { prevTime -> this.Time - prevTime } ?: 0.0
+        }
+
+    public val FixedDeltaTime: Double
+        get() {
+            check(this.IsRunning)
+            return 1.0 / 20.0
         }
 
     public constructor(mainWindow: MainWindow) {
@@ -33,19 +65,35 @@ public class Engine : AutoCloseable {
 
     public fun Run() {
         this.IsRunning = true
-        this.NumberOfFrame = 0
+        this.Tick = 0
+        this.Time = 0.0
+        this.PrevTime = null
         while (!this.MainWindow.IsClosingRequested) {
+            this.Time = this.MainWindow.Time
             this.OnFrame()
+            this.Tick++
+            this.PrevTime = this.Time
         }
         this.IsRunning = false
     }
 
     private fun OnFrame() {
         GLFW.glfwPollEvents().also { GLFW2.ThrowErrorIfNeeded() }
+
+//        var accumulator = 0.0
+//        accumulator += deltaTime
+//        while (accumulator >= fixedDeltaTime) {
+//            this.OnFixedUpdate()
+//            accumulator -= fixedDeltaTime
+//        }
+
         this.OnUpdate()
         this.OnDraw()
         GLFW.glfwSwapBuffers(this.MainWindow.NativeWindowPointer).also { GLFW2.ThrowErrorIfNeeded() }
-        this.NumberOfFrame++
+    }
+
+    private fun OnFixedUpdate() {
+
     }
 
     private fun OnUpdate() {
